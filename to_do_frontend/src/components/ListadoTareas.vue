4d0b920f9fb3a76ba7879e4c9b47229eeaef2d60
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-12-08 08:54:03 -0500

    Versión inicial del componente de listado de tareas

---
+++ to_do_frontend/src/components/ListadoTareas.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="tarea in tareas" :key="tarea.id">
+            <Tarea :tarea="tarea" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue'
+
+defineProps({
+    tareas: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped>
+</style>
--- to_do_frontend/src/components/Tarea.vue
+++ to_do_frontend/src/components/Tarea.vue
@@ -8,7 +8,7 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import { defineProps } from 'vue'
 
 const props = defineProps({
