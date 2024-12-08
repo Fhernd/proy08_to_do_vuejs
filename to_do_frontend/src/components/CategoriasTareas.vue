ce608d0eb5e6dda3d32c7a2b645ee850ad943495
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-12-08 08:58:00 -0500

    Versión inicial del componente de categorías de tareas

---
+++ to_do_frontend/src/components/CategoriasTareas.vue
@@ -0,0 +1,16 @@
+<template>
+    <button @click="filtrarTareas('todas')">Todas las tareas</button>
+    <button @click="filtrarTareas('pendientes')">Pendientes</button>
+    <button @click="filtrarTareas('completadas')">Finalizadas</button>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const filtrarTareas = (filtro) => {
+    console.log('Filtrar tareas', filtro)
+}
+</script>
+
+<style scoped></style>
