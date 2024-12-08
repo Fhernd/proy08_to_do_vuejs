2694eb929bd634a6fb5e0d99b06894d6afdce219
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-12-08 08:51:22 -0500

    Diseño inicial del componente de Tarea.

---
+++ to_do_frontend/src/components/Tarea.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <input type="checkbox" :checked="tarea.completada" @change="completarTarea" />
+            <span :class="{ completada: tarea.completada }">{{ tarea.texto }}</span>
+            <button @click="eliminarTarea">Eliminar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    tarea: {
+        type: Object,
+        required: true
+    },
+})
+
+const completarTarea = () => {
+    props.tarea.completada = !props.tarea.completada
+}
+
+const eliminarTarea = () => {
+    console.log('Eliminar tarea')
+}
+
+</script>
+
+<style scoped></style>
