d05bf14403e5764ded281d4006bef1045df78659
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-12-09 05:58:43 -0500

    Versión inicial del componente de formulario para agregar tareas

---
+++ to_do_frontend/src/components/FormularioAgregacion.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" v-model="contenidoTarea" @keyup.enter="agregarTarea"
+            placeholder="Ingresa una tarea y presiona Enter" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contenidoTarea: '',
+            tareas: []
+        };
+    },
+    methods: {
+        agregarTarea() {
+            if (this.contenidoTarea.trim() !== '') {
+                this.tareas.push({ name: this.contenidoTarea, status: 'pending' });
+                this.contenidoTarea = '';
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+input {
+    width: 100%;
+    padding: 10px;
+    margin: 10px 0;
+    box-sizing: border-box;
+}
+</style>
