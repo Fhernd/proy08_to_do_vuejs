d5b08f855cc7f5175caf515ebb2b12c580d9b1a8
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-12-16 05:38:52 -0500

    El ícono del formulario queda en el extremo derecho

--- to_do_frontend/src/components/FormularioAgregacion.vue
+++ to_do_frontend/src/components/FormularioAgregacion.vue
@@ -2,9 +2,9 @@
     <div class="flex items-center justify-center mt-4">
         <div class="relative w-full max-w-lg">
             <input type="text" placeholder="Ingresa una tarea y presiona Enter"
-                class="w-full px-4 py-2 pl-10 border rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                v-model="nuevaTarea" @keyup.enter="agregarTarea" />
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                class="w-full px-4 py-2 pr-10 border rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                v-model="contenidoTarea" @keyup.enter="agregarTarea" />
+            <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                 <i class="fas fa-pencil-alt text-gray-400"></i>
             </div>
         </div>
