5c62c283d0f4de789c5ea09278f80b9bea27cfcd
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-12-21 05:57:35 -0500

    Inclusión de alias para incorporar recursos en Vue.js creados por nosotros mismos

--- to_do_frontend/vite.config.js
+++ to_do_frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // Configura el alias para el directorio "src"
+    },
+  },
   css: {
     postcss: './postcss.config.js', // Asegura que Vite utiliza PostCSS para procesar Tailwind CSS
   },
-})
+});
