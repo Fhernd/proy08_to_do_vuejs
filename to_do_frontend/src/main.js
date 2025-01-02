b93730559c05cab408653bbe407fc7214baa8c48
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-01-01 19:25:31 -0500

    Incorporación de Toastification

--- to_do_frontend/src/main.js
+++ to_do_frontend/src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css';
 
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const options = {
+}
+
+App.use(Toast, options);
+
 createApp(App).mount('#app')
